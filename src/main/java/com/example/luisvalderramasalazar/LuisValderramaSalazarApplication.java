5c129a4f0bedaa6c9d39f21994436a1825b38e3b
Luis Valderrama <lvalderramas2@upao.edu.pe>
2022-12-31 12:15:24 -0500

    creacion proyecto

---
+++ src/main/java/com/example/luisvalderramasalazar/LuisValderramaSalazarApplication.java
@@ -0,0 +1,13 @@
+package com.example.luisvalderramasalazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuisValderramaSalazarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuisValderramaSalazarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luisvalderramasalazar/LuisValderramaSalazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luisvalderramasalazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuisValderramaSalazarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
